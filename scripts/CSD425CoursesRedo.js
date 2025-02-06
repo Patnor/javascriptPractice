a5cbd58eaf8ad77a0f45c3b222fe4ebbe4b7f9c7
Patnor <patnor22@gmail.com>
2025-02-06 13:02:13 -0800

    Completed a working page load

--- scripts/CSD425CoursesRedo.js
+++ scripts/CSD425CoursesRedo.js
@@ -1,32 +1,76 @@
 function init(){
+    // pg 550 fetch info
     const data = fetch('../data/CSD425Courses.json')
-            .then((response) => {
-                console.log(response.status);
-                console.log(response.bodyUsed);
-                console.log(response.text);
-                return response.json();
-            })
-            .then(result => {
-                const tBody = document.getElementById("tbl_courses");
+        .then((response) => {
+            console.log(response.status);
+            console.log(response.bodyUsed);
+
+            return response.json();
+        })
+        .then(result => {
+
+            // pg 285
+            // this is to ensure the quarters are in order
+            result.sort((val1, val2) => {
+                if(val1.quarter < val2.quarter)
+                    return -1;
+                else if(val1.quarter > val2.quarter)
+                    return 1;
+                else return 0;
+            });
+            //console.log(result.length);
+            const tBody = document.getElementById("tbl_courses");
+            
+            // result.sort((val1, val2) => {
+            //     if(val1.name < val2.name) return -1;
+            //     else if(val1.name > val2.name) return 1;
+            //     else return 0;
+            // })
+
+            // Set up the first quarter.
+            const trq = document.createElement('tr');
+            const tdq = document.createElement('td');
+
+            tdq.textContent = "Quarter 1";
+            trq.appendChild(tdq);
+            trq.className = 'quarter';     
+            tBody.appendChild(trq);
+
+            let currentQuarter = 1;
+
+            for(let i = 0; i < result.length; i++){
+                   
+                if(currentQuarter != result[i].quarter){
+                    const trq = document.createElement('tr');
+                    const tdq = document.createElement('td');
+                    currentQuarter = result[i].quarter;
+                    tdq.textContent = `Quarter ${currentQuarter}`;
+                    trq.appendChild(tdq);
+                    trq.className = 'quarter';     
+                    tBody.appendChild(trq);
+                }
+
                 const tr = document.createElement('tr');
-                const td = document.createElement('td')
-               // td.textContent = result[0].id + ": " + result[0].name + " (credits " + result[0].credits + ")";
-                td.textContent = `${result[0].id}:   ${result[0].name}  (${result[0].credits} credits)`;
+                const td = document.createElement('td');
+                td.textContent = `${result[i].id}:   ${result[i].name}  (${result[i].credits} credits)`;
                 tr.appendChild(td);
                 tr.className = 'unavailable';
+                // pg 417
                 tr.addEventListener('click', clicked);
                 tBody.appendChild(tr);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+            }
     
-
-    //console.log(response.json());
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
+
+
 
 function clicked(){
-
+    alert("Clicked");
 }
 
+// pg. 418
 document.addEventListener('DOMContentLoaded',init);
